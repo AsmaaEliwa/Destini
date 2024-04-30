a644074d07d72039f935ad27f7a75efe37363ef8
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2024-04-30 12:13:46 -0400

    done

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,22 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let story0 = "You see a fork in the road"
 
+    var storyBrain = StoryBrain()
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = story0
-        choice1Button.setTitle("Take a left.", for: .normal)
-        choice2Button.setTitle("Take a right.", for: .normal)
-        choice1Button.titleLabel?.text = "whatever"
+        udateTheView()
+      
     }
 
     @IBAction func choiceButtonTapped(_ sender: UIButton) {
+        if sender.titleLabel?.text == storyBrain.stories[storyBrain.numberOfStory].choice1{
+            storyBrain.goToTiggerStory()
+        }else{
+            storyBrain.goToTresureStory()
+        }
+        udateTheView()
     }
-    
+    func udateTheView(){
+        storyLabel.text = storyBrain.stories[storyBrain.numberOfStory].title
+        choice1Button.setTitle(storyBrain.stories[storyBrain.numberOfStory].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyBrain.numberOfStory].choice2, for: .normal)
+    }
 }
 
